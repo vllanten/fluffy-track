086492c35d8ece06e92a531a3062a03535adae24
Victor Hugo Llanten Galaz <victor@debianVL>
2018-05-07 19:44:27 -0300

    m

--- src/app/main/track/track.controller.js
+++ src/app/main/track/track.controller.js
@@ -10,12 +10,21 @@
     var vm = this;
     vm.marcadores = [];
 
+
+
     vm.dispositivos = [];
     vm.selectedDispositivos = [];
 
     var today = new Date();
     vm.dateInicio = today;
     vm.dateFin = today;
+
+    vm.minDate = today;
+    vm.maxDate = today;
+
+    vm.changeDate = function(){
+      vm.minDate = new Date(vm.dateInicio)
+    }
 
     function getGrupos() {
       api.gps.grupos({},
@@ -38,14 +47,28 @@
 
     function getPuntos(center, zoom) {
 
-      var inicial = ""
       var final = new Date(vm.dateFin.valueOf() + vm.dateFin.getTimezoneOffset() * 60000);
 
+      var rango_fin = moment(final).format('YYYY-MM-DD HH:mm:ss');
+
+      if (vm.tipo_busqueda == 2 ){
+        //búsqueda por fechas
+        var rango_inicio = moment(vm.dateInicio).format('YYYY-MM-DD') + " 00:00:00";
+      }else{
+        //búsqueda por intervalo
+        //ahora
+        var _now = new Date();
+        //ahora GTM 0
+        var now = new Date(_now.valueOf() + _now.getTimezoneOffset() * 60000);
+        //ahora GTM 0 - el intervalo
+        var rango_inicio = moment(now).subtract(vm.selectedIntervalo, 'minutes').format('YYYY-MM-DD HH:mm:ss');
+      }
+
+      console.log(rango_inicio)
       var query = {
-        // limit: 10,
         id_dispositivo: vm.selectedDispositivos ,
-        fecha_ini: moment(vm.dateInicio).format('YYYY-MM-DD'),
-        fecha_fin:  moment(final).format('YYYY-MM-DD HH:mm:ss'),
+        fecha_ini: rango_inicio,
+        fecha_fin:  rango_fin,
       };
       api.gps.puntos(query,
         function (response) {
